e38e875f70a357a4c39f9ad1582656ba48cb9c93
salsabildhn <salsabildhn@student.uns.ac.id>
2023-04-10 03:30:58 +0700

    Add existing project files to Git

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "jmpl_signup"
+})
+
+app.post('/signup', (req, res) => {
+    const sql = "INSERT INTO login (`name`, `email`, `password`) VALUES (?)";
+    const values = [
+        req.body.name,
+        req.body.email,
+        req.body.password
+    ]
+    db.query(sql, [values], (err, data) => {
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+app.listen(8080, () => {
+    console.log("listening");
+})
